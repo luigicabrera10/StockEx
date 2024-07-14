44afa558853ee9ea6a232bdae3228d4ce74258d6
luigicabrera10 <luigi.cabrera.h@hotmail.com>
2024-07-14 18:10:22 -0500

    On branch main Changes to be committed:
    	modified:   Smart-Contract-Template-main/io/src/lib.rs
    			Implementation of Operation Struct + actions, querys and events
    			Double hash for active and closed operations. May change on future to just one hash
    			May change to date datatype
    
    	modified:   Smart-Contract-Template-main/src/lib.rs
    			Implementation of contract basic actions

--- Smart-Contract-Template-main/io/src/lib.rs
+++ Smart-Contract-Template-main/io/src/lib.rs
@@ -1,7 +1,137 @@
+// #![no_std]
+
+// use gmeta::{In, InOut, Metadata};
+// use gstd::{prelude::*, ActorId};
+
+// pub struct ProgramMetadata;
+
+// // 1. Define actions, events, errors and state for your metadata.
+// impl Metadata for ProgramMetadata {
+//     type Init = In<InitStruct>;
+//     type Handle = InOut<Actions, Result<Events, Errors>>;
+//     type Others = ();
+//     type Reply = ();
+//     type Signal = ();
+//     type State = InOut<Query, QueryReply>;
+// }
+
+// // 2. Create your init Struct(Optional)
+// #[derive(Decode, Encode, TypeInfo)]
+// #[codec(crate = gstd::codec)]
+// #[scale_info(crate = gstd::scale_info)]
+// pub struct InitStruct {
+//     // Example:
+//     pub ft_program_id: ActorId,
+// }
+
+// // 3. Create your own Actions
+// #[derive(Debug, Decode, Encode, TypeInfo)]
+// #[codec(crate = gstd::codec)]
+// #[scale_info(crate = gstd::scale_info)]
+// pub enum Actions {
+//     // Add Actions
+//     FirstAction,               // Example an action with a simple input
+//     SecondAction(String),      // Example an action with String input
+//     ThirdAction(u128),         // Example an action with a u128 input
+//     Fourthaction(CustomInput), // Example an action with a custom input
+//     Fifthaction {
+//         // Example an action with a custom input
+//         first_field: u128,
+//         second_field: Vec<ActorId>,
+//     },
+// }
+
+// // Example of a custom input for an action
+// #[derive(Debug, Decode, Encode,  Clone, TypeInfo)]
+// #[codec(crate = gstd::codec)]
+// #[scale_info(crate = gstd::scale_info)]
+// pub struct CustomInput {
+//    pub firstfield: String,
+//    pub secondfield: u128,
+//    pub thirdfield: ActorId,
+// }
+
+// // 4. Create your own Events
+// #[derive(Debug, Decode, Encode, TypeInfo)]
+// #[codec(crate = gstd::codec)]
+// #[scale_info(crate = gstd::scale_info)]
+// pub enum Events {
+//     // Add Events(Example)
+//     FirstEvent,          // Example an event with a simple input
+//     SecondEvent(String), // Example an event with a u128 input
+//     ThirdEvent(u128),    // Example an event with String input
+//     FourtEvent {
+//         first_field: ActorId,
+//         second_field: Vec<ActorId>, // Example an event with a custom input
+//     },
+// }
+
+// // 5. Create your own Errors
+// #[derive(Debug, Decode, Encode, TypeInfo)]
+// #[codec(crate = gstd::codec)]
+// #[scale_info(crate = gstd::scale_info)]
+// pub enum Errors {
+//     // Add errors(Example)
+//     FirstError,
+//     SecondError,
+//     ThirdErrors,
+//     FourtErrors,
+// }
+
+// // 6. Create your own Struct
+// #[derive(Debug, Decode, Encode, TypeInfo)]
+// #[codec(crate = gstd::codec)]
+// #[scale_info(crate = gstd::scale_info)]
+// pub struct IoCustomStruct {
+//     pub firstfield: String,
+//     pub secondfield: String,
+//     pub thirdfield: u128,
+//     pub fourthfield: Vec<(ActorId, CustomInput)> ,
+//     pub fifthfield: Vec<(ActorId, u128)> ,
+// }
+
+
+// // 7. Create your State Querys
+// #[derive(Encode, Decode, TypeInfo)]
+// #[codec(crate = gstd::codec)]
+// #[scale_info(crate = gstd::scale_info)]
+// pub enum Query {
+//     All,
+//     FirstField,
+//     SecondField,
+//     ThirdField,
+//     FourthField{ actor_id: ActorId },
+//     FifthField{ actor_id: ActorId }
+    
+// }
+
+// // 8. Create your State Query Replys
+// #[derive(Encode, Decode, TypeInfo)]
+// #[codec(crate = gstd::codec)]
+// #[scale_info(crate = gstd::scale_info)]
+// pub enum QueryReply {
+//     All(IoCustomStruct),
+//     FirstField(String),
+//     SecondField(String),
+//     ThirdField(u128),
+//     FourthField(Option<CustomInput>),
+//     FifthField(Option<u128>)
+ 
+// }
+
+
+
 #![no_std]
 
 use gmeta::{In, InOut, Metadata};
 use gstd::{prelude::*, ActorId};
+// use chrono::{DateTime, Utc};
+
+// use decimal::d128;
+// use decimal::Decimal;
+
+// use rust_decimal::Decimal;
+// use rust_decimal_macros::dec;
 
 pub struct ProgramMetadata;
 
@@ -30,25 +160,20 @@ pub struct InitStruct {
 #[scale_info(crate = gstd::scale_info)]
 pub enum Actions {
     // Add Actions
-    FirstAction,               // Example an action with a simple input
-    SecondAction(String),      // Example an action with String input
-    ThirdAction(u128),         // Example an action with a u128 input
-    Fourthaction(CustomInput), // Example an action with a custom input
-    Fifthaction {
-        // Example an action with a custom input
-        first_field: u128,
-        second_field: Vec<ActorId>,
-    },
+    OpenOperation(OpenOperationInput),  // Example an action
+    CloseOperation(u128),               // Example an action
+    CloseAllOperations,
 }
+
 
 // Example of a custom input for an action
 #[derive(Debug, Decode, Encode,  Clone, TypeInfo)]
 #[codec(crate = gstd::codec)]
 #[scale_info(crate = gstd::scale_info)]
-pub struct CustomInput {
-   pub firstfield: String,
-   pub secondfield: u128,
-   pub thirdfield: ActorId,
+pub struct OpenOperationInput {
+   pub tickerSymbol: String, // Like TSL, FB, MSFT
+   pub operationType: bool,  // 0 = BUY operation, 1 = SELL operation
+   pub investment: u128,
 }
 
 // 4. Create your own Events
@@ -57,13 +182,9 @@ pub struct CustomInput {
 #[scale_info(crate = gstd::scale_info)]
 pub enum Events {
     // Add Events(Example)
-    FirstEvent,          // Example an event with a simple input
-    SecondEvent(String), // Example an event with a u128 input
-    ThirdEvent(u128),    // Example an event with String input
-    FourtEvent {
-        first_field: ActorId,
-        second_field: Vec<ActorId>, // Example an event with a custom input
-    },
+    OperationCreated,   // Example an event with a simple input
+    OperationClosed, // Example an event with a u128 input
+    AllOperationsClosed, // Example an event with a u128 input
 }
 
 // 5. Create your own Errors
@@ -72,22 +193,33 @@ pub enum Events {
 #[scale_info(crate = gstd::scale_info)]
 pub enum Errors {
     // Add errors(Example)
-    FirstError,
-    SecondError,
-    ThirdErrors,
-    FourtErrors,
+    OpeningOperationError,
+    ClosingOperationError,
 }
 
 // 6. Create your own Struct
-#[derive(Debug, Decode, Encode, TypeInfo)]
+// #[derive(Debug, Decode, Encode, TypeInfo)]
+// #[codec(crate = gstd::codec)]
+// #[scale_info(crate = gstd::scale_info)]
+// pub struct AllOperationsQuery {
+//     pub activeOperations: Vec<Operation>,
+//     pub closedOperations: Vec<Operation>,
+// }
+
+
+// Create Operation Struct
+#[derive(Debug, Clone, Decode, Encode, TypeInfo)]
 #[codec(crate = gstd::codec)]
 #[scale_info(crate = gstd::scale_info)]
-pub struct IoCustomStruct {
-    pub firstfield: String,
-    pub secondfield: String,
-    pub thirdfield: u128,
-    pub fourthfield: Vec<(ActorId, CustomInput)> ,
-    pub fifthfield: Vec<(ActorId, u128)> ,
+pub struct Operation {
+    pub id: u128,
+    pub tickerSymbol: String,
+    pub operationType: bool,
+    pub openDate: String,  // may change
+    pub closeDate: String, // may change
+    pub investment: u128,
+    pub openPrice: u128,
+    pub closedPrice: u128,
 }
 
 
@@ -96,13 +228,9 @@ pub struct IoCustomStruct {
 #[codec(crate = gstd::codec)]
 #[scale_info(crate = gstd::scale_info)]
 pub enum Query {
-    All,
-    FirstField,
-    SecondField,
-    ThirdField,
-    FourthField{ actor_id: ActorId },
-    FifthField{ actor_id: ActorId }
-    
+    // All,
+    ActiveOperations,
+    ClosedOperations,    
 }
 
 // 8. Create your State Query Replys
@@ -110,11 +238,8 @@ pub enum Query {
 #[codec(crate = gstd::codec)]
 #[scale_info(crate = gstd::scale_info)]
 pub enum QueryReply {
-    All(IoCustomStruct),
-    FirstField(String),
-    SecondField(String),
-    ThirdField(u128),
-    FourthField(Option<CustomInput>),
-    FifthField(Option<u128>)
+    // All(AllOperationsQuery),
+    ActiveOperations(Vec<Operation>),
+    ClosedOperations(Vec<Operation>),
  
 }
--- Smart-Contract-Template-main/src/lib.rs
+++ Smart-Contract-Template-main/src/lib.rs
@@ -1,3 +1,156 @@
+// #![no_std]
+// use gstd::{async_main, collections::HashMap, msg, prelude::*, ActorId};
+// use io::*;
+
+// // 1. Create the main state as a static variable.
+// static mut STATE: Option<CustomStruct> = None;
+
+// // Create a public State
+// #[derive(Clone, Default)]
+// pub struct CustomStruct {
+//     pub firstfield: String,
+//     pub secondfield: String,
+//     pub thirdfield: u128,
+//     pub fourthfield: HashMap<ActorId, CustomInput> ,
+//     pub fifthfield: HashMap<ActorId, u128>,
+// }
+
+// // Create a implementation on State
+// impl CustomStruct {
+//     fn firstmethod(&mut self) -> Result<Events, Errors> {
+//         // Update your state with a String input
+//         self.firstfield = "Hello".to_string();
+
+//         Ok(Events::FirstEvent)
+//     }
+
+//     async fn secondmethod(&mut self, input: String) -> Result<Events, Errors> {
+//         // Update your state with a String input
+//         self.secondfield = input.clone();
+
+//         Ok(Events::SecondEvent(input))
+//     }
+
+//     async fn thirdmethod(&mut self, input: u128) -> Result<Events, Errors> {
+//         // Update your state with a u128 input
+//         self.thirdfield = input;
+
+//         Ok(Events::ThirdEvent(input))
+//     }
+
+//     async fn fourthmethod(&mut self, input: CustomInput) -> Result<Events, Errors> {
+//         // Update your state.
+//         self.fourthfield 
+//             .entry(msg::source())
+//             .or_insert(CustomInput {
+//                 firstfield: input.firstfield,
+//                 secondfield: input.secondfield,
+//                 thirdfield: input.thirdfield,
+//             });
+
+//         Ok(Events::SecondEvent("Event".to_string()))
+//     }
+
+//     async fn fifthmethod(
+//         &mut self,
+//         _first_field: u128,
+//         _second_field: Vec<ActorId>,
+//     ) -> Result<Events, Errors> {
+//         // Update your state.
+//         self.fifthfield
+//             .entry(msg::source())
+//             .and_modify(|number| *number = number.saturating_add(1))
+//             .or_insert(1);
+
+//         Ok(Events::SecondEvent("Event".to_string()))
+//     }
+// }
+
+// // 3. Create the init() function of your contract.
+// #[no_mangle]
+// extern "C" fn init() {
+//     let config: InitStruct = msg::load().expect("Unable to decode InitStruct");
+
+//     if config.ft_program_id.is_zero() {
+//         panic!("InitStruct program address can't be 0");
+//     }
+
+//     let state = CustomStruct {
+//         ..Default::default()
+//     };
+
+//     unsafe { STATE = Some(state) };
+// }
+
+// // 4.Create the main() function of your contract.
+// #[async_main]
+// async fn main() {
+//     // We load the input message
+//     let action: Actions = msg::load().expect("Could not load Action");
+
+//     let state: &mut CustomStruct =
+//         unsafe { STATE.as_mut().expect("The contract is not initialized") };
+
+//     // We receive an action from the user and update the state. Example:
+//     let reply = match action {
+//         Actions::FirstAction => state.firstmethod(), // Here, we call the implementation
+//         Actions::SecondAction(input) => state.secondmethod(input).await, // Here, we call the implementation
+//         Actions::ThirdAction(input) => state.thirdmethod(input).await, // Here, we call the implementation
+//         Actions::Fourthaction(input) => state.fourthmethod(input).await, // Here, we call the implementation
+//         Actions::Fifthaction {
+//             first_field,
+//             second_field, 
+//         } => state.fifthmethod(first_field, second_field).await,
+//     };
+//     msg::reply(reply, 0).expect("Error in sending a reply");
+// }
+
+// // 5. Create the state() function of your contract.
+// #[no_mangle]
+// extern "C" fn state() {
+//     let state = unsafe { STATE.take().expect("Unexpected error in taking state") };
+//     let query: Query = msg::load().expect("Unable to decode the query");
+//     let reply = match query {
+//         Query::All => QueryReply::All(state.into()),
+//         Query::FirstField => QueryReply::FirstField(state.firstfield.clone()),
+//         Query::SecondField => QueryReply::SecondField(state.secondfield.clone()),
+//         Query::ThirdField => QueryReply::ThirdField(state.thirdfield.clone()),
+//         Query::FourthField{actor_id} => QueryReply::FourthField(state.fourthfield.get(&actor_id).cloned()),
+//         Query::FifthField{actor_id} => QueryReply::FifthField(state.fifthfield.get(&actor_id).cloned()),
+//     };
+
+//     msg::reply(reply, 0).expect("Error on sharinf state");
+// }
+
+// // Implementation of the From trait for converting CustomStruct to IoCustomStruct
+// impl From<CustomStruct> for IoCustomStruct {
+//     // Conversion method
+//     fn from(value: CustomStruct) -> Self {
+//         // Destructure the CustomStruct object into its individual fields
+//         let CustomStruct {
+//             firstfield,
+//             secondfield,
+//             thirdfield,
+//             fourthfield,
+//             fifthfield,
+//         } = value;
+
+//         // Perform some transformation, cloning its elements
+//         let fourthfield = fourthfield.into_iter().collect();
+//         let fifthfield = fifthfield.into_iter().collect();
+
+//         // Create a new IoCustomStruct object using the destructured fields
+//         Self {
+//             firstfield,
+//             secondfield,
+//             thirdfield,
+//             fourthfield,
+//             fifthfield,
+//         }
+//     }
+// }
+
+
 #![no_std]
 use gstd::{async_main, collections::HashMap, msg, prelude::*, ActorId};
 use io::*;
@@ -8,63 +161,91 @@ static mut STATE: Option<CustomStruct> = None;
 // Create a public State
 #[derive(Clone, Default)]
 pub struct CustomStruct {
-    pub firstfield: String,
-    pub secondfield: String,
-    pub thirdfield: u128,
-    pub fourthfield: HashMap<ActorId, CustomInput> ,
-    pub fifthfield: HashMap<ActorId, u128>,
+    pub activeOperations: HashMap<ActorId, Vec<(Operation)> >,
+    pub closedOperations: HashMap<ActorId, Vec<(Operation)> >,
 }
 
 // Create a implementation on State
 impl CustomStruct {
-    fn firstmethod(&mut self) -> Result<Events, Errors> {
-        // Update your state with a String input
-        self.firstfield = "Hello".to_string();
 
-        Ok(Events::FirstEvent)
+    async fn newOperation(&mut self, input: OpenOperationInput) -> Result<Events, Errors> {
+        
+        let actor_id = msg::source();
+
+        // If the actor_id doesn't exist in activeOperations, create a new entry with an empty vector
+        let operations = self.activeOperations.entry(actor_id).or_insert(Vec::new());
+
+        // Create a new operation with the provided input and an id of 0
+        let new_operation = Operation {
+            id: 0,  // You may want to implement a proper ID generation logic
+            tickerSymbol: input.tickerSymbol,
+            operationType: input.operationType,
+            // openDate: Utc::now().to_string(),
+            openDate: "START DATE".to_string(),
+            closeDate: String::new(),  // Empty string as it is not closed yet
+            investment: input.investment,
+            // openPrice: 0.0,  // Initialize as needed
+            // closedPrice: 0.0,  // Initialize as needed
+            openPrice: 0,  // Initialize as needed
+            closedPrice: 0,  // Initialize as needed
+        };
+
+        // Push the new operation to the vector of operations
+        operations.push(new_operation);
+
+        Ok(Events::OperationCreated)
+
     }
 
-    async fn secondmethod(&mut self, input: String) -> Result<Events, Errors> {
-        // Update your state with a String input
-        self.secondfield = input.clone();
-
-        Ok(Events::SecondEvent(input))
+    async fn finishOperation(&mut self, operation_id: u128) -> Result<Events, Errors> {
+        let actor_id = msg::source();
+    
+        // Check if the actor_id exists in activeOperations
+        if let Some(operations) = self.activeOperations.get_mut(&actor_id) {
+            // Find the operation with the given operation_id
+            if let Some(index) = operations.iter().position(|op| op.id == operation_id) {
+                let operation = operations.remove(index);
+    
+                // Modify operation fields as needed, e.g., setting closeDate
+                let mut operation_cloned = operation.clone();
+                operation_cloned.closeDate = "CLOSED DATE".to_string();
+    
+                // Insert the operation into closedOperations
+                let closed_operations = self.closedOperations.entry(actor_id).or_insert(Vec::new());
+                closed_operations.push(operation_cloned);
+    
+                Ok(Events::OperationClosed)
+            } else {
+                Err(Errors::ClosingOperationError)
+            }
+        } else {
+            Err(Errors::ClosingOperationError)
+        }
     }
 
-    async fn thirdmethod(&mut self, input: u128) -> Result<Events, Errors> {
-        // Update your state with a u128 input
-        self.thirdfield = input;
-
-        Ok(Events::ThirdEvent(input))
-    }
-
-    async fn fourthmethod(&mut self, input: CustomInput) -> Result<Events, Errors> {
-        // Update your state.
-        self.fourthfield 
-            .entry(msg::source())
-            .or_insert(CustomInput {
-                firstfield: input.firstfield,
-                secondfield: input.secondfield,
-                thirdfield: input.thirdfield,
-            });
-
-        Ok(Events::SecondEvent("Event".to_string()))
-    }
-
-    async fn fifthmethod(
-        &mut self,
-        _first_field: u128,
-        _second_field: Vec<ActorId>,
-    ) -> Result<Events, Errors> {
-        // Update your state.
-        self.fifthfield
-            .entry(msg::source())
-            .and_modify(|number| *number = number.saturating_add(1))
-            .or_insert(1);
-
-        Ok(Events::SecondEvent("Event".to_string()))
+    async fn closeAll(&mut self) -> Result<Events, Errors> {
+        let actor_id = msg::source();
+    
+        // Move all active operations to closed operations
+        if let Some(mut operations) = self.activeOperations.remove(&actor_id) {
+            let closed_operations = self.closedOperations.entry(actor_id).or_insert(Vec::new());
+    
+            for operation in operations.drain(..) {
+                // Modify operation fields as needed, e.g., setting closeDate
+                let mut cloned_operation = operation.clone();
+                cloned_operation.closeDate = "CLOSED DATE".to_string();
+                closed_operations.push(cloned_operation);
+            }
+    
+            Ok(Events::AllOperationsClosed)
+        } else {
+            Err(Errors::ClosingOperationError)
+        }
     }
 }
+
+
+
 
 // 3. Create the init() function of your contract.
 #[no_mangle]
@@ -93,14 +274,11 @@ async fn main() {
 
     // We receive an action from the user and update the state. Example:
     let reply = match action {
-        Actions::FirstAction => state.firstmethod(), // Here, we call the implementation
-        Actions::SecondAction(input) => state.secondmethod(input).await, // Here, we call the implementation
-        Actions::ThirdAction(input) => state.thirdmethod(input).await, // Here, we call the implementation
-        Actions::Fourthaction(input) => state.fourthmethod(input).await, // Here, we call the implementation
-        Actions::Fifthaction {
-            first_field,
-            second_field, 
-        } => state.fifthmethod(first_field, second_field).await,
+
+        Actions::OpenOperation(input) => state.newOperation(input).await, // Here, we call the implementation
+        Actions::CloseOperation(input) => state.finishOperation(input).await, // Here, we call the implementation
+        Actions::CloseAllOperations => state.closeAll().await, // Here, we call the implementation
+
     };
     msg::reply(reply, 0).expect("Error in sending a reply");
 }
@@ -111,41 +289,44 @@ extern "C" fn state() {
     let state = unsafe { STATE.take().expect("Unexpected error in taking state") };
     let query: Query = msg::load().expect("Unable to decode the query");
     let reply = match query {
-        Query::All => QueryReply::All(state.into()),
-        Query::FirstField => QueryReply::FirstField(state.firstfield.clone()),
-        Query::SecondField => QueryReply::SecondField(state.secondfield.clone()),
-        Query::ThirdField => QueryReply::ThirdField(state.thirdfield.clone()),
-        Query::FourthField{actor_id} => QueryReply::FourthField(state.fourthfield.get(&actor_id).cloned()),
-        Query::FifthField{actor_id} => QueryReply::FifthField(state.fifthfield.get(&actor_id).cloned()),
+        // Query::All => QueryReply::All(state.into()),
+        Query::ActiveOperations => { // read wallet return tag
+            let answer: Vec<Operation> = state.activeOperations
+                .get(&msg::source())
+                .cloned() // Clone the contents of Option<&Vec<Operation>> to Vec<Operation>
+                .unwrap_or_else(|| Vec::new()); // Handle None case if necessary
+            QueryReply::ActiveOperations(answer)
+        },
+        Query::ClosedOperations => { // read wallet return tag
+            let answer: Vec<Operation> = state.closedOperations
+                .get(&msg::source())
+                .cloned() // Clone the contents of Option<&Vec<Operation>> to Vec<Operation>
+                .unwrap_or_else(|| Vec::new()); // Handle None case if necessary
+            QueryReply::ClosedOperations(answer)
+        },
     };
 
     msg::reply(reply, 0).expect("Error on sharinf state");
 }
 
 // Implementation of the From trait for converting CustomStruct to IoCustomStruct
-impl From<CustomStruct> for IoCustomStruct {
-    // Conversion method
-    fn from(value: CustomStruct) -> Self {
-        // Destructure the CustomStruct object into its individual fields
-        let CustomStruct {
-            firstfield,
-            secondfield,
-            thirdfield,
-            fourthfield,
-            fifthfield,
-        } = value;
+// impl From<CustomStruct> for AllOperationsQuery {
+//     // Conversion method
+//     fn from(value: CustomStruct) -> Self {
+//         // Destructure the CustomStruct object into its individual fields
+//         let CustomStruct {
+//             activeOperations,
+//             closedOperations,
+//         } = value;
 
-        // Perform some transformation, cloning its elements
-        let fourthfield = fourthfield.into_iter().collect();
-        let fifthfield = fifthfield.into_iter().collect();
+//         // Perform some transformation, cloning its elements
+//         let activeOperations = activeOperations.into_iter().collect();
+//         let closedOperations = closedOperations.into_iter().collect();
 
-        // Create a new IoCustomStruct object using the destructured fields
-        Self {
-            firstfield,
-            secondfield,
-            thirdfield,
-            fourthfield,
-            fifthfield,
-        }
-    }
-}
+//         // Create a new IoCustomStruct object using the destructured fields
+//         Self {
+//             activeOperations,
+//             closedOperations,
+//         }
+//     }
+// }
