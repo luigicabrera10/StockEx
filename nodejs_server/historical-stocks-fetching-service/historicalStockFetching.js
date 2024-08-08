257479c19c6dd178be9e965ab9165325d62ad561
luigicabrera10 <luigi.cabrera.h@hotmail.com>
2024-08-08 14:35:12 -0500

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   .gitignore
    		Added more api keys
    
    	modified:   nodejs_server/allStocks.txt
    		Changed a stock that was not supported
    	modified:   nodejs_server/historical-stocks-fetching-service/historicalStockFetching.js
    		Hnadles the historical prices of all supported stocks
    	modified:   nodejs_server/historical-stocks-fetching-service/savedHistoricalStocksPrices.json
    		Saves al historycal prices needed

--- nodejs_server/historical-stocks-fetching-service/historicalStockFetching.js
+++ nodejs_server/historical-stocks-fetching-service/historicalStockFetching.js
@@ -1,112 +1,56 @@
 const axios = require('axios');
 const fs = require('fs').promises;
 
+// Will be 1 day every time
 let historicalPricesUpdates = {};
 /* Example { 
    'TSLA': {
-      '1min': {
-         'lastRefresh': '2024-07-24T23:59:59Z', 
-         'serie': { ... }
-      }
-      '5min': {
-         'lastRefresh': '2024-07-22T23:59:59Z', 
-         'serie': { ... }
-      }
-      '15min': {
-         'lastRefresh': '2024-07-15T23:59:59Z', 
-         'serie': { ... }
-      }
-      '30min': {
-         'lastRefresh': '2024-06-27T23:59:59Z', 
-         'serie': { ... }
-      }
-      '60min': {
-         'lastRefresh': '2024-07-24T23:59:59Z', 
-         'serie': { ... }
-      }
-      'days': {
-         'lastRefresh': '2024-07-24T23:59:59Z', 
-         'serie': { ... }
-      }
-      'weeks': {
-         'lastRefresh': '2024-07-24T23:59:59Z', 
-         'serie': { ... }
-      }
-      'months': {
-         'lastRefresh': '2024-07-24T23:59:59Z', 
-         'serie': { ... }
-      }
+      'lastRefresh': '2024-07-24T23:59:59Z', 
+      'serie': [ 
+         { 
+            "datetime": "2024-08-08",
+            "open": "213.09500",
+            "high": "213.46001",
+            "low": "208.83000",
+            "close": "213.37500",
+            "volume": "21241581"
+         },
+         {
+            "datetime": "2004-09-28",
+            "open": "0.66893",
+            "high": "0.68375",
+            "low": "0.66875",
+            "close": "0.67929",
+            "volume": "353186400"
+         }
+      ]
    }
    'MSFT': {
-      '1min': {
-         'lastRefresh': '2024-07-24T23:59:59Z', 
-         'serie': { ... }
-      }
-      '5min': {
-         'lastRefresh': '2024-07-24T23:59:59Z', 
-         'serie': { ... }
-      }
-      '15min': {
-         'lastRefresh': '2024-07-24T23:59:59Z', 
-         'serie': { ... }
-      }
-      '30min': {
-         'lastRefresh': '2024-07-24T23:59:59Z', 
-         'serie': { ... }
-      }
-      '60min': {
-         'lastRefresh': '2024-07-24T23:59:59Z', 
-         'serie': { ... }
-      }
-      'days': {
-         'lastRefresh': '2024-07-24T23:59:59Z', 
-         'serie': { ... }
-      }
-      'weeks': {
-         'lastRefresh': '2024-07-24T23:59:59Z', 
-         'serie': { ... }
-      }
-      'months': {
-         'lastRefresh': '2024-07-24T23:59:59Z', 
-         'serie': { ... }
-      }
+      'lastRefresh': '2024-07-24T23:59:59Z', 
+      'serie': [ ... ]
    }
 }
 
 */
 
 
-let HISTORICAL_API_KEYS = [];
-let HISTORICAL_API_KEYS_INDEX = 0;
-let allSupportedHistoricalStocks = ['TSLA'/*, 'MSFT', 'IBM'*/];
+let HISTORICAL_API_KEY;
+let allSupportedHistoricalStocks = ['TSLA', 'MSFT', 'IBM'];
+let nextUpdate = new Date().getTime();
 
 const allSupportedHistoricalStocksFile = '/home/northsoldier/Documents/Hackathons/Varathon - StockEx/nodejs_server/allStocks.txt';
-const historicalStockApiKeyFile = '/home/northsoldier/Documents/Hackathons/Varathon - StockEx/nodejs_server/historical-stocks-fetching-service/historicalStockApiKey.txt';
+const historicalStockApiKeyFile = '/home/northsoldier/Documents/Hackathons/Varathon - StockEx/nodejs_server/historical-stocks-fetching-service/historicalStockApiKeyTwelveData.txt';
 const savedHistoricalStockPricesFile = '/home/northsoldier/Documents/Hackathons/Varathon - StockEx/nodejs_server/historical-stocks-fetching-service/savedHistoricalStocksPrices.json';
 
-const validIntervals = ['1min','5min','15min','30min','60min'];
-const validPeriods = {'days': 'TIME_SERIES_DAILY', 'weeks': 'TIME_SERIES_WEEKLY','months': 'TIME_SERIES_MONTHLY'};
-
-const secsxday = 86400;
-const updateHistoricalStocksTimeRate = {
-   '1min': 1000,           // every 1 minutes
-   '5min': 1000 * 5,       // every 10 minutes
-   '15min': 1000 * 15,     // every 15 minutes
-   '30min': 1000 * 30,     // every 30 minutes
-   '60min': 1000 * 60,     // every 60 minutes
-   'days': secsxday,       // every day
-   'weeks': secsxday*7,    // every 7 days
-   'months': secsxday*30.  // every 30 days
-}; 
+const updateRateHistoricalStocks = 24 * 60 * 60 * 1000;
 
 
 async function setHistoricalStockApiKey() {
    try {
       const pass = await fs.readFile(historicalStockApiKeyFile, 'utf-8');
-      HISTORICAL_API_KEYS = pass.trim(); // Trim any extra whitespace
-      HISTORICAL_API_KEYS = HISTORICAL_API_KEYS.split("\n");
+      HISTORICAL_API_KEY =  pass.trim(); // Trim any extra whitespace
    } catch (error) {
-      console.error("Error reading historical API key:", error);
+      console.error("Error reading passphrase:", error);
       throw error; // Handle or propagate the error as needed
    }
 }
@@ -122,19 +66,19 @@ async function loadSavedHistoricalStockPrices() {
 
 async function saveHistoricalStockPrices() {
    try {
-       const data = JSON.stringify(historicalPricesUpdates, null);
-       await fs.writeFile(savedHistoricalStockPricesFile, data, 'utf-8');
+      const data = JSON.stringify(historicalPricesUpdates, null, 2);
+      await fs.writeFile(savedHistoricalStockPricesFile, data, 'utf-8');
    } catch (error) {
-       console.error("Error saving the stock prices to file \"", savedHistoricalStockPricesFile, "\":", error);
+      console.error("Error saving the stock prices to file \"", savedHistoricalStockPricesFile, "\":", error);
    }
 }
 
 async function loadAllHistoricalSupportedStocks() {
    try {
-       const data = await fs.readFile(allSupportedHistoricalStocksFile, 'utf-8');
-       allSupportedHistoricalStocks = JSON.parse(data)
+      const data = await fs.readFile(allSupportedHistoricalStocksFile, 'utf-8');
+      allSupportedHistoricalStocks = JSON.parse(data)
    } catch (error) {
-       console.error("Error reading the supported stocks file \"", allSupportedHistoricalStocksFile, "\":", error);
+      console.error("Error reading the supported stocks file \"", allSupportedHistoricalStocksFile, "\":", error);
    }
 }
 
@@ -143,89 +87,152 @@ function isStockHistorySupported(symbol){
 }
 
 
-async function getHistory(symbol, period) {
+async function callHistoryPriceApi(symbol) {
 
-   let url;
-   if (validPeriods[period]) {
-      url = `https://www.alphavantage.co/query?function=${validPeriods[period]}&symbol=${symbol}&outputsize=full&apikey=${HISTORICAL_API_KEYS[HISTORICAL_API_KEYS_INDEX]}`;
-   } else if (validIntervals.includes(period)) {
-      url = `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${symbol}&interval=${period}&outputsize=full&apikey=${HISTORICAL_API_KEYS[HISTORICAL_API_KEYS_INDEX]}`;
-   } else {
-      console.log("Invalid Period/Interval");
-      return null;
+   if (!isStockHistorySupported(symbol)) {
+      console.log("NOT SUPPORTED SYMBOL: ", symbol);
+      return false;
    }
+
+   const url = `https://api.twelvedata.com/time_series?symbol=${symbol}&interval=1day&outputsize=5000&apikey=${HISTORICAL_API_KEY}`;
 
    try {
       const response = await axios.get(url, {
          headers: {'User-Agent': 'axios'}
       });
-      // console.log("Data Fetched Successfully!");
+
+      // console.log("Data Fetched Successfully: ", response.data);
 
       const keys = Object.keys(response.data);
-      if (keys.includes("Information")) {
-         HISTORICAL_API_KEYS_INDEX = (HISTORICAL_API_KEYS_INDEX + 1) % HISTORICAL_API_KEYS.length;
-         console.log("Trying api key number ", HISTORICAL_API_KEYS_INDEX+1);
-         return await getHistory(symbol, period);
+
+      // Handle api limit rate
+      if (keys.includes("message")) {
+         console.log("API ERROR ON SYMBOL '", symbol,": ", response.data.message);
+         return false;
       }
 
-      return response.data;
+      return response.data.values;
    } catch (error) {
       console.error('Error fetching data:', error);
-      return {};
+      return false;
    }
+   return true;
 }
 
-async function updateHistoricalPrices(symbol, periods){
+async function updateHistoricalPrices(symbol){
 
    if (!isStockHistorySupported(symbol)){
       console.log("Not Supported symbol");
-      return;
+      return false;
    }
 
    const currentTime = new Date().getTime();
 
-   periods.forEach( async period => { // for every period
+   let update = false;
+   if (!historicalPricesUpdates[symbol]) update = true;
+   else{
+      const lastRefreshTime = new Date(historicalPricesUpdates[symbol]['lastRefresh']).getTime();
+      if (currentTime - lastRefreshTime > updateRateHistoricalStocks) update = true;
+   }
 
-      let update = false
+   if (!update) return true;
 
-      if (!historicalPricesUpdates[symbol] || !historicalPricesUpdates[symbol][period]) update = true;
-      else{
-         const lastRefreshTime = new Date(historicalPricesUpdates[symbol][period]['lastRefresh'].lastRefresh).getTime();
-         if (currentTime - lastRefreshTime > updateHistoricalStocksTimeRate[period]) update = true;
+   console.log("Warning: Updating the serie of '", symbol, "'");
+
+   try{
+
+      const dataSerie = await callHistoryPriceApi(symbol);
+      // console.log("Response: ", dataSerie);
+
+      if (dataSerie === false) {
+         console.log("No Data, no update");
+         return false;
       }
 
-      if (!update) return;
+      const currentDate = new Date();
+      
+      // Get the keys of the main object
+      const keys = Object.keys(dataSerie);
+      // const headerString = keys[1];
 
-      console.log("Warning: Updating the serie of '", symbol, "' on '", period, "' period");
+      historicalPricesUpdates[symbol] = {
+         'lastRefresh': currentDate.toISOString(), 
+         'serie': dataSerie
+      };
 
-      try{
+      // console.log("Refresh: ", JSON.stringify(historicalPricesUpdates[symbol]).substring(0, 500));
 
-         const dataSerie = await getHistory(symbol, period);
-         const currentDate = new Date();
+   }
+   catch (error) {
+      console.error('Error:', error);
+      return false
+   }
 
-         console.log("response: ", dataSerie);
-         
-         // Get the keys of the main object
-         const keys = Object.keys(dataSerie);
-         const headerString =  keys[1];
-
-         if (!historicalPricesUpdates[symbol]) historicalPricesUpdates[symbol] = {};
-         historicalPricesUpdates[symbol][period] = {
-            'lastRefresh': currentDate.toISOString(), 
-            'serie': dataSerie[headerString]
-         };
-
-         console.log("Refresh: ", JSON.stringify(historicalPricesUpdates[symbol][period]).substring(0, 500));
-
-      }
-      catch (error) {
-         console.error('Error:', error);
-      }
-
-   });
 
    await saveHistoricalStockPrices();
+   return true;
+}
 
+
+async function getPriceHistory(symbol){
+   if (!isStockHistorySupported(symbol)){
+      console.log("Not Supported symbol");
+      return null;
+   }
+
+   if (!historicalPricesUpdates[symbol]) {
+      console.log("Not symbol not found on database");
+      return null;
+   }
+
+   await updateHistoricalPrices(symbol);
+   return historicalPricesUpdates[symbol]['serie'];
+}
+
+
+async function wait(ms) {
+   return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function updateStocksDaily() {
+   while (true) {
+      const currentTime = new Date().getTime();
+      let allUpdated = true;
+      let state;
+
+      for (let stock of allSupportedHistoricalStocks) {
+         const lastRefreshTime = historicalPricesUpdates[stock]
+            ? new Date(historicalPricesUpdates[stock]['lastRefresh']).getTime()
+            : 0;
+
+         if (currentTime - lastRefreshTime > updateRateHistoricalStocks) {
+            allUpdated = false;
+            state = await updateHistoricalPrices(stock);
+            // if (!state) console.log("Failed to update stock: ", stock);
+
+
+            // Pause to respect the API rate limit (8 calls per minute)
+            await wait(7500); // 7.5 seconds delay between each call
+         }
+
+         // If the API limit is hit (8 calls), wait for 65 seconds
+         if (Object.keys(historicalPricesUpdates).length % 8 === 0 || state === false) {
+            console.log("API limit hit. Waiting for 61 seconds...");
+            await wait(61000);
+         }
+      }
+
+      if (allUpdated) {
+         console.log("All stocks are updated. Waiting until the next day...");
+         // Wait until the next day (the next update cycle)
+         const timeUntilNextDay = updateRateHistoricalStocks - (currentTime % updateRateHistoricalStocks);
+         await wait(timeUntilNextDay);
+      } else {
+         console.log("Some stocks were updated. Continuing process...");
+         // If not all stocks were updated, continue checking after a short break
+         await wait(5000); // 5 seconds delay before the next loop iteration
+      }
+   }
 }
 
 
@@ -233,20 +240,26 @@ async function initHistoricalStockFetchingService() {
    console.log("Starting Historical Stock Fetching Service\n");
 
    await setHistoricalStockApiKey();
-   // await loadAllSupportedStocks(); 
+   await loadAllHistoricalSupportedStocks(); 
    await loadSavedHistoricalStockPrices();
 
    console.log("Saved Historical Prices loaded: ", historicalPricesUpdates, "\n");
 
-   // Examples:
-   // const historyData = await getHistory("MSFT", "days");
-   // console.log(historyData);
-
-   // 
-   allSupportedHistoricalStocks.forEach( async stock => {
-      await updateHistoricalPrices(stock, ['days']);
-   });
+   updateStocksDaily(); // Start the infinite update loop
    
+   // Exmpales
+   // console.log("Get history: ", await getPriceHistory("MSFT"));
+   // console.log("Get history: ", await getPriceHistory("BABA"));
+
 }
 
-initHistoricalStockFetchingService();
+// initHistoricalStockFetchingService();
+
+
+module.exports = {
+   initHistoricalStockFetchingService,
+   isStockHistorySupported,
+   getPriceHistory,
+   updateHistoricalPrices,
+};
+
